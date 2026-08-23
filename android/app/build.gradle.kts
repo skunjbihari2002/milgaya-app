ea984efd23e0629afa666fa4085bba65e167a1d0
Kunj <skunjbihari2002@gmail.com>
2026-08-23 21:12:15 +0530

    Fix Android build for Firebase (set minSdk 23)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.milgaya.milgaya"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
         // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
