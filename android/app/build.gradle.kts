3a53b2771dbc1749e7a919ea155db925edb9c791
Kunj <skunjbihari2002@gmail.com>
2026-08-24 15:07:09 +0530

    Fix Android Gradle minSdk 23 and coreLibraryDesugaring for Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -22,7 +23,7 @@ android {
         applicationId = "com.milgaya.milgaya"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
         // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
@@ -54,4 +55,7 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
 }
